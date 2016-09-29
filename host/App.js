75acc85469941b50a54c0ddaad912e459be8009a
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-09-29 15:08:10 +0900

    rename

--- host/App.js
+++ host/App.js
@@ -37,8 +37,8 @@ class App extends Component {
       for(var i = 0; i < Object.keys(pairs).length * 2; i++) datas[i] = []
       for(var key in pairs){
         var base = (parseInt(key) - 1) * 2
-        datas[base + 0] = [key, pairs[key].user1, "User1"].concat(pairs[key].logs.map(obj => (obj.answer1 == "yes")? "自白する" : "自白しない").reverse())
-        datas[base + 1] = [  "", pairs[key].user2, "User2"].concat(pairs[key].logs.map(obj => (obj.answer2 == "yes")? "自白する" : "自白しない").reverse())
+        datas[base + 0] = [key, pairs[key].user1, "彼"].concat(pairs[key].logs.map(obj => (obj.answer1 == "yes")? "待つ" : "出掛ける").reverse())
+        datas[base + 1] = [  "", pairs[key].user2, "彼女"].concat(pairs[key].logs.map(obj => (obj.answer2 == "yes")? "待つ" : "出掛ける").reverse())
       }
     }
     console.log(datas)
@@ -58,9 +58,9 @@ class App extends Component {
         <ConfigEditor />
         <MatchingButton />
         <DownloadButton
-          fileName={"prisoners_dilemma.csv"}
+          fileName={"battle-of-sexes.csv"}
           list={[
-            ["囚人のジレンマ"],
+            ["デートゲーム"],
             ["実験日", new Date()],
             ["登録者数", users? Object.keys(users).length : 0],
             ["グループ数", pairs? Object.keys(pairs).length : 0],
